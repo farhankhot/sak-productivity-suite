e0e8c62a4146af6288b023bbe4a0c6a0ef7270b6
Farhan K <farhan.crazyeyes@gmail.com>
2023-03-28 23:05:04 -0400

    changes

--- backend/app/server.py
+++ backend/app/server.py
@@ -435,6 +435,6 @@ def save_cookie():
 @app.errorhandler(404)
 def not_found(e):
     print("not found, error 404")
-    print(os.listdir("."))
-    return render_template("client/build/index.html")
+    print(os.listdir("./client"))
+    return render_template("./client/build/index.html")
 # ================================================ ROUTES END =============================================
