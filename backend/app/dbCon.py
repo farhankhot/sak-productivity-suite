83e3d785fc587ad3f863c48cdd59781e5989e7fd
Farhan K <farhan.crazyeyes@gmail.com>
2023-03-28 20:03:28 -0400

    changes

--- backend/app/dbCon.py
+++ backend/app/dbCon.py
@@ -49,9 +49,7 @@ def get_cookie_from_user_sessions(session_id):
     try:
         cursor = connection.cursor()
         print(session_id)
-        # Remove variable injection
-        select_query = """ SELECT cookie FROM socialmedia.user_sessions WHERE session_id = '"""+session_id+"""'; """
-        cursor.execute(select_query)
+        cursor.execute('SELECT cookie FROM socialmedia.user_sessions WHERE session_id=?', (session_id))
         connection.commit()
         if cursor.fetchone() is not None :
             return cursor.fetchone()
