f68262c514877406340e2929270feb8e14227d8d
Farhan K <farhan.crazyeyes@gmail.com>
2023-03-29 00:03:07 -0400

    Added static_url_path='' in Flask object

--- server.py
+++ server.py
@@ -34,6 +34,7 @@ q = Queue(connection=conn)
 
 app = Flask(
     __name__,
+    static_url_path='',
     static_folder="client/build")
 
 # Enables CORS (this is only needed when working with React.js, I don't know why)
