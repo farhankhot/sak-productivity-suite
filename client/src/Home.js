e2655bc1aba7cdb51844422b1c6f64c9630439cd
Farhan K <farhan.crazyeyes@gmail.com>
2023-04-27 13:32:51 -0400

    changes

--- client/src/Home.js
+++ client/src/Home.js
@@ -116,17 +116,16 @@ function Home(props) {
 
 				CheckJobStatus(jobIdArray.message[i], (resultArray) => {
 
-					// === Change ===
 					console.log("Successfully gotten Connect note array: ", resultArray);
 					connectNoteArray[i] = resultArray;
 					setShowProfileArea(true);
-					// === Change ===
+
+					peopleInterestsButtonDisabled[i] = false;
+					companyInterestsButtonDisabled[i] = false;
+					makingConnectNoteButtonDisabled[i] = false;
+					sendingConnectNoteButtonDisabled[i] = false;
 					
 					if (i === (jobIdArray.message.length - 1) ){
-						peopleInterestsButtonDisabled[i] = false;
-						companyInterestsButtonDisabled[i] = false;
-						makingConnectNoteButtonDisabled[i] = false;
-						sendingConnectNoteButtonDisabled[i] = false;
 						setIsLoadingAutoCreatingNotes(false);
 						setLoadingLeadsButtonDisabled(false);
 						setAutoCreatingNotesDisabled(false);
