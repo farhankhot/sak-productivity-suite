9f1afe115dc8e703878b01970c6e1a963462ee93
Farhan K <farhan.crazyeyes@gmail.com>
2023-05-11 13:58:14 -0400

    changes

--- client/src/Home.js
+++ client/src/Home.js
@@ -199,6 +199,7 @@ function Home(props) {
 								const job_list = data.job_list;
 								console.log(job_list);
 								for (let j = 0; j < job_list.length; j++){
+									console.log(j);
 									if (job_list[j]["status"] === "finished"){
 										// Get its idx and display it
 										console.log(job_list[j]["idx"], job_list[j]["result"]);
